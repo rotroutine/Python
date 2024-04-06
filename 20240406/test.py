224eb0c3e2d5ad6579d3d6a8929b7c02c8186faf
Kanbe__Kotori <rotroutine@qq.com>
2024-04-06 23:12:17 +0800

    20240406

---
+++ 20240406/test.py
@@ -0,0 +1,67 @@
+# age = int(input("please enter your girlfriend's age: "))
+
+# if age < 12:
+#     print("萝莉控死刑！")
+#     print("只是我喜欢的人刚好是萝莉而已！")
+# elif age >= 12 and age < 18:
+#     print("三年起步！")
+#     print("最高死刑！")
+# else:
+#     print("没意思，不考虑！")
+
+# aim = 50
+
+# # 通过if-elif-else语句来实现有次数的猜数字游戏
+# if int(input("please enter a number: ")) == aim:
+#     print("you are right!")
+# elif int(input("please enter a number: ")) == aim:
+#     print("you are right!")
+# elif int(input("please enter a number: ")) == aim:
+#     print("you are right!")
+# else:
+#     print("you have no chance!")
+
+# sum = 0
+# count = 1
+
+# while count <= 100:
+#     sum += count
+#     count += 1
+
+# print(sum)
+
+# num = 51
+
+# while True:
+#     guess = int(input("please guess a number: "))
+
+#     if (guess == num):
+#         print("you are right!")
+#         break
+#     elif (guess < num):
+#         print("too small!")
+#     else:
+#         print("too big!")
+
+# print("Hello", end = " ")
+# print("World!")
+
+# i = 1
+# while i <= 9:
+#     j = 1
+#     while j <= i:
+#         print(f"{j} * {i} = {i * j}", end = " ")
+#         j += 1
+    
+#     print()
+#     i += 1
+
+# test = "hello world"
+
+# # print(test[0:6])
+# for ch in test:
+#     print(ch, end = " ")
+
+# for x in range(0, 101, 2):
+#     print(x, end = " ")
+# print()
---
+++ Test.py
@@ -0,0 +1,36 @@
+# print(3 ** 2)
+# print(int(3 / 2))
+# print(float(3 // 2))
+# print(3 % -2)
+
+# input = int(input("Enter a number: "))
+# print(input)
+
+# """
+# who are you
+# """
+# print(1)
+
+# temp = """who are you"""
+# print(temp)
+
+# temp = #who are you
+# print(temp)
+
+# temp =  1
+# print("this is" + str(temp) + "a test")
+
+# message = "I am so %s, and i wanna sleep %.1f hours" %("sleepy", 0.55)
+# print(message)
+
+# #include <stdio.h>
+
+# int main() {
+#     printf("%.1lf\n", 0.55);
+#     return 0;
+# }
+
+status = "sleepy"
+hours = 0.55
+
+print(f"I am so {status}, and i wanna sleep {hours:.1f} hours")

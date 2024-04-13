c7665d6553c637024aa2469395ae5835ba15452a
Kanbe__Kotori <rotroutine@qq.com>
2024-04-13 23:57:28 +0800

    20240413

---
+++ 20240413/Test.py
@@ -0,0 +1,50 @@
+# set = {1, 2, 3, "str", (1, 2, 3), [3.14, 5.12], {1: "one", 2: "two"}}
+
+# for element in set:
+#     print(element)
+
+#* Python中的set是通过哈希表来实现的，所以set中的元素是无序的，且不重复
+
+# set = {"hello", 1.23, 512}
+
+# for element in set:
+#     print(element)
+
+# print(set)
+
+# dict = dict()
+
+# dict.keys()
+
+# set = {[1,2,3], (1,2,3), {1,2,3}}
+# set = {(1,2,3)}
+
+# print(set)
+
+# set = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+# print(len(set))
+# print(max(set))
+# print(min(set))
+
+# print(list(set), type(list(set)))
+
+# list = [1,2,2,1]
+# print(set(list))
+
+# str1 = "b"
+# str2 = "aaaaaaa"
+# if str1 > str2:
+#     print("str1 > str2")
+# else:
+#     print("str1 < str2")
+
+# x, y, z = 1, 2, 3
+# print(x, y, z)
+
+# def test(*args) -> None:
+#     print(args)
+    
+# test(1,2.13, (12,13), [1,"hello"], {1: "one", 2: "two"})
+
+# file = open("test.txt", "w")
+# file.
